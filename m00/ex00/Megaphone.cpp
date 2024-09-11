6eb110ebe845d501ae39c3466beaa8f2b32e6327
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-09-11 15:55:23 +0200

    initial commit

---
+++ m00/ex00/Megaphone.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc == 0) {
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    return 0;
+}
