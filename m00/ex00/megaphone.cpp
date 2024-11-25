a34f2fe796784ed21063bb59156267d409fc6b44
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-11-25 13:28:38 +0100

    fix megaphone

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
 #include <cctype>
-#include <locale>
+#include <iostream>
 #include <string>
-#include <vector>
 
-
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-    {
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
-    else
-    {
-        std::wstring input(argv[1]);
-        for (int i = 1; i < argc; i++)
-        {
+    } else {
+        std::string input(argv[1]);
+        for (int i = 1; i < argc; i++) {
             for (const char *p = argv[i]; *p; ++p) {
-                std::wcout << static_cast<char>(std::toupper(*p));
+                std::cout << static_cast<char>(std::toupper(*p));
             }
         }
     }
