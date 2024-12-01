9caf9e27133b5733ee7323629958713948f53684
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2024-12-01 21:02:20 +0100

    m02/ex03: more tests

--- m02/ex03/main.cpp
+++ m02/ex03/main.cpp
@@ -4,12 +4,18 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
+void test(Point const a, Point const b, Point const c, Point const point) {
+    std::cout << "Triangle: " << a << b << c << std::endl;
+    std::cout << point << " is inside: " << bsp(a, b, c, point) << std::endl << std::endl;
+}
+
 int main() {
     Point p1(1.0f, 2.0f);
     Point p2(4.0f, 4.0f);
     Point p3(1.0f, 5.0f);
-    Point p4(1.0f, 5.0f);
-
-    std::cout << "Triangle: " << p1 << p2 << p3 << std::endl;
-    std::cout << p4 << " is inside: " << bsp(p1, p2, p3, p4) << std::endl;
+    test(p1, p2, p3, Point(1.0f, 1.5f));
+    test(p1, p2, p3, Point(1.0f, 4.0f));
+    test(p1, p2, p3, Point(1.01f, 4.0f));
+    test(p1, p2, p3, Point(1.8f, 3.8f));
+    test(p1, p2, p3, Point(3.0f, 2.9f));
 }
