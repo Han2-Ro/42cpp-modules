b8e606ffe19663b44040eda7af21d12da10ec598
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-01-23 14:04:39 +0100

    m07/ex00: implement max & min

--- m07/ex00/main.cpp
+++ m07/ex00/main.cpp
@@ -3,6 +3,19 @@
 
 #include "whatever.hpp"
 
+struct Data {
+    int         value;
+    std::string str;
+};
+
+bool operator<(const Data& lhs, const Data& rhs) {
+    return lhs.value < rhs.value;
+}
+
+bool operator>(const Data& lhs, const Data& rhs) {
+    return lhs.value > rhs.value;
+}
+
 int main() {
     {
         int a = 3;
@@ -11,11 +24,37 @@ int main() {
         swap(a, b);
         std::cout << "a: " << a << ", b: " << b << std::endl;
     }
+    std::cout << std::endl;
     {
         std::string a = "hello";
         std::string b = "world";
         std::cout << "a: " << a << ", b: " << b << std::endl;
         swap(a, b);
         std::cout << "a: " << a << ", b: " << b << std::endl;
+    }
+    std::cout << std::endl;
+    {
+        Data a = {1, "hello"};
+        Data b = {2, "world"};
+        Data min_data = min(a, b);
+        std::cout << "min_data: " << min_data.value << ", " << min_data.str << std::endl;
+        Data max_data = max(a, b);
+        std::cout << "max_data: " << max_data.value << ", " << max_data.str << std::endl;
+    }
+    std::cout << std::endl;
+    {
+        Data a = {1, "hello"};
+        Data b = {1, "world"};
+        Data min_data = min(a, b);
+        std::cout << "min_data: " << min_data.value << ", " << min_data.str << std::endl;
+        Data max_data = max(a, b);
+        std::cout << "max_data: " << max_data.value << ", " << max_data.str << std::endl;
+    }
+    std::cout << std::endl;
+    {
+        int a = 42;
+        int b = -42;
+        std::cout << "min(a, b): " << min(a, b) << std::endl;
+        std::cout << "max(a, b): " << max(a, b) << std::endl;
     }
 }
--- m07/ex00/whatever.hpp
+++ m07/ex00/whatever.hpp
@@ -5,3 +5,13 @@ void swap(T& a, T& b) {
     a = b;
     b = temp;
 }
+
+template <typename T>
+T min(T a, T b) {
+    return a < b ? a : b;
+}
+
+template <typename T>
+T max(T a, T b) {
+    return a > b ? a : b;
+}
