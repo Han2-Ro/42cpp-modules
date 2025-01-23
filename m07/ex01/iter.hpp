d12b4651e94501d7098ba7f0ca7a6af0c17e68e8
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-01-23 14:15:55 +0100

    m07/ex01: iter

---
+++ m07/ex01/iter.hpp
@@ -0,0 +1,7 @@
+#include <cstddef>
+template <typename T>
+void iter(T* arr, size_t size, void (*f)(T&)) {
+    for (size_t i = 0; i < size; i++) {
+        f(arr[i]);
+    }
+}
---
+++ m07/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "iter.hpp"
+
+void print(int& num) {
+    std::cout << num << std::endl;
+}
+
+void add_one(int& num) {
+    num += 1;
+}
+
+void double_num(int& num) {
+    num *= 2;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    iter(arr, 5, print);
+    iter(arr, 5, add_one);
+    std::cout << std::endl;
+    iter(arr, 5, print);
+    iter(arr, 5, double_num);
+    std::cout << std::endl;
+    iter(arr, 5, print);
+    return 0;
+}
