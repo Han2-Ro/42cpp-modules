100b1c5b72658154e6faea4447524722ef921c30
hannes <hannes.rother@online.de>
2025-02-07 13:47:56 +0100

    m07/ex00

---
+++ m08/ex00/easyfind.hpp
@@ -0,0 +1,11 @@
+#include <exception>
+#include <algorithm>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::exception();
+    return it;
+}
