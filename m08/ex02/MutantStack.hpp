bfd36b880dea459e517f20b59199b266c6ac3828
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-03-23 12:26:29 +0100

    m08/ex02: inters

--- m08/ex02/MutantStack.hpp
+++ m08/ex02/MutantStack.hpp
@@ -1,12 +1,17 @@
 #pragma once
 #include <algorithm>
+#include <stack>
 
 template <typename T>
-class MutantStack: public std::stack<T> {
+class MutantStack : public std::stack<T> {
    public:
-    MutantStack();
-    MutantStack(const MutantStack& other);
-    MutantStack& operator=(const MutantStack& other);
-    ~MutantStack();
+    class iterator : public std::iterator<T> {
+    }
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    ~MutantStack() {}
+    iterator begin() {
+        
+    }
+    iterator end() {}
 };
-
