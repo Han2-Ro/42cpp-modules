32d1176166b40887d56e90cfdd69a1b4b6eaf0d9
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-05-14 11:50:24 +0200

    m09/ex02: correct binary_insert

--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -4,14 +4,17 @@
 #include <vector>
 
 #include "PmergeMe.hpp"
+unsigned int comparasions_counter = 0;
 
 void binary_insert(std::vector<unsigned int>& vec, unsigned int n) {
-    std::size_t begin = 0;
+    // exclusive
+    std::size_t begin = -1;
+    // exclusive
     std::size_t end = vec.size();
     std::size_t middle = 0;
-    int comparasions_counter = 0;
     while (end - begin > 1) {
         middle = (begin + end) / 2;
+        std::cout << middle << "_";
         comparasions_counter++;
         if (n < vec.at(middle)) {
             end = middle;
@@ -19,13 +22,13 @@ void binary_insert(std::vector<unsigned int>& vec, unsigned int n) {
             begin = middle;
         }
     }
-    comparasions_counter++;
-    if (vec.size() == 0 || n < vec.at(begin)) {
-        vec.insert(vec.begin() + begin, n);
-    } else {
-        vec.insert(vec.begin() + end, n);
-    }
-    std::cout << comparasions_counter << std::endl;
+    // if (vec.size() == 0 || (std::cout << begin << " ", comparasions_counter++, n < vec.at(begin))) {
+    //     vec.insert(vec.begin() + begin, n);
+    // } else {
+    //     vec.insert(vec.begin() + end, n);
+    // }
+    vec.insert(vec.begin() + end, n);
+    std::cout << std::endl;
 }
 
 void print_vec(std::vector<unsigned int>& vec) {
@@ -58,7 +61,8 @@ void test_is_sorted() {
         vec.push_back(1);
         assert(!is_sorted(vec));
     }
-    { std::vector<unsigned int> vec; 
+    {
+        std::vector<unsigned int> vec;
         vec.push_back(45);
         vec.push_back(1);
         assert(!is_sorted(vec));
@@ -88,9 +92,11 @@ void run_tests() {
 int main(int argc, char** argv) {
     run_tests();
     std::vector<unsigned int> vec;
+    comparasions_counter = 0;
     for (int i = 1; i < argc; i++) {
         unsigned int n = std::strtoul(argv[i], nullptr, 10);
         binary_insert(vec, n);
-        print_vec(vec);
     }
+    print_vec(vec);
+    std::cout << "comparisions: " << comparasions_counter << std::endl;
 }
