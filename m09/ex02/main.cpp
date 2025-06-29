05d5993f92119faf4b7dd33f2e34592e95f5be17
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-06-29 19:45:48 +0200

    sort vec and deque in main

--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -4,20 +4,38 @@
 
 #include "PmergeMe.hpp"
 
+template <typename Container>
+bool is_sorted(Container vec) {
+    for (typename Container::iterator iter = vec.begin(); iter + 1 < vec.end(); iter++) {
+        if (*iter > *(iter + 1)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char** argv) {
-    std::vector<unsigned int> vec;
+    std::vector<unsigned int> input;
     for (int i = 1; i < argc; i++) {
         unsigned int n = std::strtoul(argv[i], NULL, 10);
-        vec.push_back(n);
+        input.push_back(n);
     }
+    std::vector<unsigned int> result = sort_vector(input);
+    if (!is_sorted(result)){
+        return 1;
+    }
+
     std::deque<unsigned int> dq;
     for (int i = 1; i < argc; i++) {
         unsigned int n = std::strtoul(argv[i], NULL, 10);
         dq.push_back(n);
     }
-    comparasions_counter = 0;
-    print_vec(dq);
     dq = sort_deque(dq);
-    print_vec(dq);
-    std::cout << "comparisions: " << comparasions_counter << std::endl;
+    if (!is_sorted(dq)){
+        return 1;
+    }
+    std::cout << "Before: ";
+    print_vec(input);
+    std::cout << "After: ";
+    print_vec(result);
 }
