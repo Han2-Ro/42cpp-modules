ee25462d59f3b5d15dc8f582918bb1c47a786964
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-06-29 13:08:53 +0200

    add deque in main and print_vec template

--- m09/ex02/PmergeMe.hpp
+++ m09/ex02/PmergeMe.hpp
@@ -9,12 +9,12 @@
 extern unsigned int       comparasions_counter;
 std::vector<unsigned int> sort_merge_insert(std::vector<unsigned int>& vec);
 
-template <typename T>
-void print_vec(const std::vector<T>& vec) {
+template <typename Container>
+void print_vec(const Container& vec) {
     if (vec.size() == 0) {
         std::cout << "Empty";
     }
-    for (typename std::vector<T>::const_iterator iter = vec.begin(); iter != vec.end(); iter++) {
+    for (typename Container::const_iterator iter = vec.begin(); iter != vec.end(); iter++) {
         std::cout << *iter << ",";
     }
     std::cout << std::endl;
--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <deque>
 
 #include "PmergeMe.hpp"
 
@@ -9,7 +10,13 @@ int main(int argc, char** argv) {
         unsigned int n = std::strtoul(argv[i], NULL, 10);
         vec.push_back(n);
     }
+    std::deque<unsigned int> dq;
+    for (int i = 1; i < argc; i++) {
+        unsigned int n = std::strtoul(argv[i], NULL, 10);
+        dq.push_back(n);
+    }
     comparasions_counter = 0;
+    print_vec(dq);
     vec = sort_merge_insert(vec);
     print_vec(vec);
     std::cout << "comparisions: " << comparasions_counter << std::endl;
