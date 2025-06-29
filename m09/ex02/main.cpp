6ec4fef379c4891d8ba7283c18c33dce2e34e247
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-06-29 19:58:35 +0200

    add timing

--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
-#include <iostream>
+#include <ctime>
 #include <deque>
+#include <iomanip>
+#include <iostream>
 
 #include "PmergeMe.hpp"
 
@@ -20,8 +22,13 @@ int main(int argc, char** argv) {
         unsigned int n = std::strtoul(argv[i], NULL, 10);
         input.push_back(n);
     }
+
+    clock_t                   start_time = clock();
     std::vector<unsigned int> result = sort_vector(input);
-    if (!is_sorted(result)){
+    clock_t                   end_time = clock();
+    double                    vector_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC * 1000000;
+
+    if (!is_sorted(result)) {
         return 1;
     }
 
@@ -30,12 +37,23 @@ int main(int argc, char** argv) {
         unsigned int n = std::strtoul(argv[i], NULL, 10);
         dq.push_back(n);
     }
+
+    start_time = clock();
     dq = sort_deque(dq);
-    if (!is_sorted(dq)){
+    end_time = clock();
+    double deque_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC * 1000000;
+
+    if (!is_sorted(dq)) {
         return 1;
     }
+
     std::cout << "Before: ";
     print_vec(input);
     std::cout << "After: ";
     print_vec(result);
+
+    std::cout << "Time to process a range of " << input.size() << " elements with std::vector : " << std::fixed
+              << vector_time << " us" << std::endl;
+    std::cout << "Time to process a range of " << input.size() << " elements with std::deque : " << std::fixed
+              << deque_time << " us" << std::endl;
 }
