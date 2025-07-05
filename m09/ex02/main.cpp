ff37f635da928374b8a5a7e245469304e6aa6dcd
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-07-05 17:23:17 +0200

    fix wrap around with negative numbers

--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -1,4 +1,6 @@
+#include <cerrno>
 #include <cstdlib>
+#include <cstring>
 #include <ctime>
 #include <deque>
 #include <iomanip>
@@ -16,21 +18,35 @@ bool is_sorted(Container vec) {
     return true;
 }
 
-int main(int argc, char** argv) {
-    std::vector<unsigned int> input;
-    char *endptr;
+template <typename Container>
+Container argv_to_container(int argc, char** argv) {
+    char* endptr;
+    Container result;
 
     for (int i = 1; i < argc; i++) {
+        if (std::strchr(argv[i], '-') != NULL) {
+            std::cerr << "Error: negative numbers not allowed: '" << argv[i] << "'" << std::endl;
+            exit(1);
+        }
         unsigned int n = std::strtoul(argv[i], &endptr, 10);
+        if (errno == ERANGE) {
+            std::cerr << "Error: number out of range: '" << argv[i] << "'" << std::endl;
+            exit(1);
+        }
         if (*endptr != '\0' || argv[i][0] == '\0') {
             std::cerr << "Error: failed to convert to unsigned long: '" << argv[i] << "'" << std::endl;
-            return 1;
+            exit(1);
         }
-        input.push_back(n);
+        result.push_back(n);
     }
+    return result;
+}
+
+int main(int argc, char** argv) {
+    std::vector<unsigned int> vec = argv_to_container<std::vector<unsigned int> >(argc, argv);
 
     clock_t                   start_time = clock();
-    std::vector<unsigned int> result = sort_vector(input);
+    std::vector<unsigned int> result = sort_vector(vec);
     clock_t                   end_time = clock();
     double                    vector_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC * 1000000;
 
@@ -38,11 +54,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    std::deque<unsigned int> dq;
-    for (int i = 1; i < argc; i++) {
-        unsigned int n = std::strtoul(argv[i], NULL, 10);
-        dq.push_back(n);
-    }
+    std::deque<unsigned int> dq = argv_to_container<std::deque<unsigned int> >(argc, argv);
 
     start_time = clock();
     dq = sort_deque(dq);
@@ -54,12 +66,12 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "Before: ";
-    print_vec(input);
+    print_vec(vec);
     std::cout << "After:  ";
     print_vec(result);
 
-    std::cout << "Time to process a range of " << input.size() << " elements with std::vector: " << std::fixed
+    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector: " << std::fixed
               << vector_time << " us" << std::endl;
-    std::cout << "Time to process a range of " << input.size() << " elements with std::deque: " << std::fixed
+    std::cout << "Time to process a range of " << vec.size() << " elements with std::deque: " << std::fixed
               << deque_time << " us" << std::endl;
 }
